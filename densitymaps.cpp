7d65cea5df2ff6378d9813b044b86c590b428881
TiagoBsCastro <tiagobscastro@github.com>
2019-12-04 14:45:18 +0100

    Update: 04/12/2019

--- densitymaps.cpp
+++ densitymaps.cpp
@@ -222,7 +222,8 @@ int testFov(double fov, double boxl, double Ds, int myid, double & fovradiants){
  void computeReplications(double fov, double boxl, double Ds, int myid, double & fovradiants, int & nrepperp){
 
   fovradiants = fov/180.*M_PI;
-  nrepperp = round( Ds * tan(fovradiants/2) / boxl );
+  nrepperp = ceil( Ds * tan(fovradiants/2) / boxl * 2 ) - 1;
+
 }
 
 /*
