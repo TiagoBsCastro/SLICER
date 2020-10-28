Bug fix: correct derivatives and padding for even grids 28/10/2020

cl.py
@@ -2,7 +2,7 @@ import numpy as np
 import pymaster as nmt
 from astropy.io import fits
 import matplotlib.pyplot as plt
-import smrv2 as smr
+import smr
 
 # This script describes the functionality of the flat-sky version of pymaster
 
@@ -15,9 +15,13 @@ Ly = 10.0 * np.pi/180
 Nx = 256
 Ny = 256
 
-mpt  = np.random.randn(Nx, Ny)
-fits.writeto("kappa.fits", mpt.astype(np.float64))
-mpt2 = smr.smr("kappa.fits")
+#mpt  = np.random.randn(Nx, Ny)
+#fits.writeto("kappa.fits", mpt.astype(np.float64))
+#mpt2 = smr.smr("kappa.fits")
+
+mpt  = fits.getdata("gamma_fd.fits")
+mpt2 = fits.getdata("gamma_fft.fits") 
+
 mask = np.ones((Nx, Ny))
 
 # Bins:

smr.py
@@ -25,7 +25,7 @@ def convergence_fft(potential, KX, KY, zero_mode=None):
 # Shear direct terms
 def shear1_fft(potential, KX, KY, zero_mode=None):
 
-    shear1_k = np.fft.fft2(potential) * (KX**2-KY**2)
+    shear1_k = np.fft.fft2(potential) * (KX**2-KY**2)/2.0
 
     if zero_mode is None:
 